9730a8e1075e144fb47ad95da775e484e1308a94
pearcej <jan_pearce@berea.edu>
2018-11-06 22:09:14 -0500

    add start files

---
+++ poaching/poaching-main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main( ){
+
+	//TODO: Add code here
+
+	char readchar;
+	cin >> readchar; //This is to keep screen open in some situations.
+
+	return 0;
+}
+
